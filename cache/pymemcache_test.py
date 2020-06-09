bfb3edfb0e883f12afb7dce85157b286349d11be
jihoon <blue_jihoon@naver.com>
2020-06-09 15:27:07 +0900

    json 매개 변수 지원 객체

--- cache/pymemcache_test.py
+++ cache/pymemcache_test.py
@@ -5,7 +5,7 @@ from pymemcache.client.base import Client
 def json_serializer(key, value):
     if isinstance(value, str):
         return value, 1
-    return json.dumps(value), 2
+    return json.dumps(str(value)), 2
 
 
 def json_deserializer(key, value, flags):
@@ -22,3 +22,8 @@ client = Client(('127.0.0.1', 11211), serializer=json_serializer,
 client.set('key', {'a': 'b', 'c': 'd'})
 result = client.get('key')
 print(result)
+
+# binary는 json encode에서 지원하지 않는 타입이므로 str(지원 가능)로 변환 후 dumps해야한다.
+bit_data = b'asdlkfajsenr'
+client.set('key1', bit_data)
+print(client.get('key1'))
